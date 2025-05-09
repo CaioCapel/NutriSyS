b9feaf82c050bdf02b577e810b0ea0d3bc5d522f
CaioCapel <caiordm1227@gmail.com>
2025-05-09 10:34:31 -0300

    Atualização do Login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,50 @@
+/*=============== SHOW HIDE PASSWORD LOGIN ===============*/
+const passwordAccess = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
+
+   iconEye.addEventListener('click', () =>{
+      // Change password to text
+      input.type === 'password' ? input.type = 'text'
+						              : input.type = 'password'
+
+      // Icon change
+      iconEye.classList.toggle('ri-eye-fill')
+      iconEye.classList.toggle('ri-eye-off-fill')
+   })
+}
+passwordAccess('password','loginPassword')
+
+/*=============== SHOW HIDE PASSWORD CREATE ACCOUNT ===============*/
+const passwordRegister = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
+
+   iconEye.addEventListener('click', () =>{
+      // Change password to text
+      input.type === 'password' ? input.type = 'text'
+						              : input.type = 'password'
+
+      // Icon change
+      iconEye.classList.toggle('ri-eye-fill')
+      iconEye.classList.toggle('ri-eye-off-fill')
+   })
+}
+passwordRegister('passwordCreate','loginPasswordCreate')
+
+/*=============== SHOW HIDE LOGIN & CREATE ACCOUNT ===============*/
+const loginAcessRegister = document.getElementById('loginAccessRegister'),
+      buttonRegister = document.getElementById('loginButtonRegister'),
+      buttonAccess = document.getElementById('loginButtonAccess')
+
+buttonRegister.addEventListener('click', () => {
+   loginAcessRegister.classList.add('active')
+})
+
+buttonAccess.addEventListener('click', () => {
+   loginAcessRegister.classList.remove('active')
+})
+
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -13,7 +60,8 @@ const fruitImages = [
   'img/banana.png',
   'img/uva.png',
   'img/morango.png',
-  'img/abacate.png'
+  'img/abacate.png',
+  'img/orange.png'
 ];
 
 
@@ -74,3 +122,4 @@ window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
+
