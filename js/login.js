a14beb858f6f27328ad8186cd69fe6fdca666671
CaioCapel <caiordm1227@gmail.com>
2025-05-09 11:36:38 -0300

    atualização login

--- js/login.js
+++ js/login.js
@@ -49,6 +49,7 @@ buttonAccess.addEventListener('click', () => {
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
